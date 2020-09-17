9a91ca11751cc9d2123fdbddb8fdab2e6f3f844e
acdra1n <rvreman7@me.com>
2020-09-17 05:04:50 +0400

    Add element.matches polyfill

--- source/wm.js
+++ source/wm.js
@@ -38,6 +38,22 @@ if (typeof Object.assign !== 'function') {
     });
 }
 
+/* Element.matches polyfill */
+
+if (!Element.prototype.matches) {
+    Element.prototype.matches = 
+        Element.prototype.matchesSelector || 
+        Element.prototype.mozMatchesSelector ||
+        Element.prototype.msMatchesSelector || 
+        Element.prototype.oMatchesSelector || 
+        Element.prototype.webkitMatchesSelector ||
+        function(s) {
+            var matches = (this.document || this.ownerDocument).querySelectorAll(s), i = matches.length;
+            while (--i >= 0 && matches.item(i) !== this) {}
+            return i > -1;            
+        };
+}
+
 /* ========================================================================================================================= */
 
 /**
